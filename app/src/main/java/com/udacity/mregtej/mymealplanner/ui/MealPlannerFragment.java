3cb10f259c61eb0e80bad5c45ffdb26d4da9579a
Modesto Rego <mregtej@gmail.com>
2019-01-13 19:48:52 +0100

    Retrieved planned meals from MealPlannerFragment

--- app/src/main/java/com/udacity/mregtej/mymealplanner/ui/MealPlannerFragment.java
+++ app/src/main/java/com/udacity/mregtej/mymealplanner/ui/MealPlannerFragment.java
@@ -1,6 +1,8 @@
 package com.udacity.mregtej.mymealplanner.ui;
 
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Parcelable;
@@ -18,10 +20,18 @@ import android.widget.CalendarView;
 import android.widget.TextView;
 
 import com.udacity.mregtej.mymealplanner.R;
+import com.udacity.mregtej.mymealplanner.datamodel.PlannedMeal;
 import com.udacity.mregtej.mymealplanner.datamodel.Recipe;
+import com.udacity.mregtej.mymealplanner.ui.adapters.MealMenuDayMealtimeAdapter;
 import com.udacity.mregtej.mymealplanner.ui.adapters.MealPlannerMealtimeAdapter;
+import com.udacity.mregtej.mymealplanner.ui.utils.DateUtils;
+import com.udacity.mregtej.mymealplanner.viewmodel.PlannedMealViewModel;
+import com.udacity.mregtej.mymealplanner.viewmodel.PlannedRecipeViewModel;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import butterknife.BindView;
@@ -34,11 +44,15 @@ import butterknife.Unbinder;
 public class MealPlannerFragment extends Fragment
         implements MealPlannerMealtimeAdapter.MealPlannerMealtimeClickListener {
 
-    /* Key for storing the list state in savedInstanceState */
-    private static final String MEALTIME_RECIPE_LIST_STATE_KEY = "mealtime-recipe-list-state";
+    /** Key for storing the list state in savedInstanceState */
+    private static final String MEAL_DAY_LIST_STATE_KEY = "meal-day-list-state";
 
-    /** Key for storing the meal-day recipes to buy in savedInstanceState */
-    private static final String MEALTIME_RECIPE_LIST_KEY = "mealtime-recipe-list";
+    /** Key for storing the meal-days to buy in savedInstanceState */
+    private static final String MEAL_DAY_LIST_KEY = "meal-day-list";
+
+    private static final String YEAR_KEY = "year";
+    private static final String MONTH_KEY = "month";
+    private static final String DAY_KEY = "day";
 
     @BindView(R.id.cv_meal_planner_calendar)
     CalendarView cvMealPlannerCalendarView;
@@ -50,6 +64,20 @@ public class MealPlannerFragment extends Fragment
     Unbinder unbinder;
     Context mContext;
     ActionBar actionBar;
+
+    private PlannedMealViewModel mPlannedMealViewModel;
+    private PlannedRecipeViewModel mPlannedRecipeViewModel;
+
+    private HashMap<String,Recipe> mMealDayList;
+    private List<PlannedMeal> mPlannedMealList;
+    private List<Recipe> mPlannedRecipeList;
+
+    private int mYear;
+    private int mMonth;
+    private int mDayOfMonth;
+
+    private boolean hasPlannedMealsRetrieved = false;
+    private boolean hasPlannedRecipesRetrieved = false;
 
     /**
      * RecyclerView LayoutManager instance
@@ -91,31 +119,50 @@ public class MealPlannerFragment extends Fragment
         rvCvMealPlannerMealtimeData.setHasFixedSize(true);
         setRecyclerViewLayoutManager(rootView);
 
+        // Subscribe MealPlansFragment to receive notifications from PlannedMealViewModel
+        registerToPlannedMealViewModel();
+
+        // Subscribe MealPlansFragment to receive notifications from PlannedRecipesViewModel
+        registerToPlannedRecipeViewModel();
+
+        // Enable notifications for receiving planned meals from PlannedMealViewModel
+        enableNotificationsFromPlannedMealViewModel();
+
+        // Enable notifications for receiving planned recipes from PlannedRecipesViewModel
+        enableNotificationsFromPlannedRecipeViewModel();
+
         if (savedInstanceState != null) {
 
-            List<Recipe> mealDayRecipes = savedInstanceState.
-                    getParcelableArrayList(MEALTIME_RECIPE_LIST_KEY);
-            mMealPlannerMealtimeAdapter = new MealPlannerMealtimeAdapter(mealDayRecipes, this);
+            mYear = savedInstanceState.getInt(YEAR_KEY);
+            mMonth = savedInstanceState.getInt(MONTH_KEY);
+            mDayOfMonth = savedInstanceState.getInt(DAY_KEY);
+
+            LinkedHashMap<String,Recipe> mealDayList =
+                    (LinkedHashMap<String,Recipe>) savedInstanceState.getSerializable(MEAL_DAY_LIST_KEY);
+            mMealPlannerMealtimeAdapter = new MealPlannerMealtimeAdapter(mealDayList, this);
             rvCvMealPlannerMealtimeData.setAdapter(mMealPlannerMealtimeAdapter);
             mMealPlannerMealtimeAdapter.notifyDataSetChanged();
+
+            // Update Date on Calendar View
+            updateCalendarView();
 
         } else {
 
-            ArrayList<Recipe> mealDayRecipes = new ArrayList<>();
-            // TODO Fill in
-            mealDayRecipes.add(null);
-            mealDayRecipes.add(null);
-            mealDayRecipes.add(null);
-            mealDayRecipes.add(null);
-            mMealPlannerMealtimeAdapter = new MealPlannerMealtimeAdapter(mealDayRecipes, this);
-            // Set Adapter and notifyDataSetChanged
-            rvCvMealPlannerMealtimeData.setAdapter(mMealPlannerMealtimeAdapter);
-            mMealPlannerMealtimeAdapter.notifyDataSetChanged();
+            // Set Current Date on Calendar View
+            setCurrentDateOnCalendarView();
 
         }
 
+        // Set-Up Calendar listeners
+        setCalendarViewListeners();
+
         // Return fragment view
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
     }
 
     private void setActionBarTitle(String title) {
@@ -148,12 +195,13 @@ public class MealPlannerFragment extends Fragment
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-
+        outState.putInt(YEAR_KEY, mYear);
+        outState.putInt(MONTH_KEY, mMonth);
+        outState.putInt(DAY_KEY, mDayOfMonth);
         mListStateMealPlannerMealtime = mMealPlannerMealtimeLayoutManager.onSaveInstanceState();
-        outState.putParcelable(MEALTIME_RECIPE_LIST_STATE_KEY, mListStateMealPlannerMealtime);
-        outState.putParcelableArrayList(MEALTIME_RECIPE_LIST_KEY,
-                (ArrayList<Recipe>) mMealPlannerMealtimeAdapter.getmMealDayRecipeList());
-
+        outState.putParcelable(MEAL_DAY_LIST_STATE_KEY, mListStateMealPlannerMealtime);
+        outState.putSerializable(MEAL_DAY_LIST_KEY, mMealPlannerMealtimeAdapter.
+                getmMealDayRecipeList());
     }
 
     @Override
@@ -161,13 +209,143 @@ public class MealPlannerFragment extends Fragment
         super.onViewStateRestored(savedInstanceState);
         if (savedInstanceState != null) {
             mListStateMealPlannerMealtime = savedInstanceState
-                    .getParcelable(MEALTIME_RECIPE_LIST_STATE_KEY);
+                    .getParcelable(MEAL_DAY_LIST_STATE_KEY);
         }
     }
 
     @Override
     public void onMealPlannerMealtimeClick(int position) {
 
+    }
+
+    private void setCurrentDateOnCalendarView() {
+        // Get current date
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(calendar.getTime());
+        mYear = calendar.get(Calendar.YEAR);
+        mMonth = calendar.get(Calendar.MONTH);
+        mDayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        // Update text-string date
+        tvMealPlannerDate.setText(DateUtils.formatDate(mYear,mMonth,mDayOfMonth,mContext));
+    }
+
+    private void setCalendarViewListeners() {
+        // Set OnDateChangeListener (user chooses a different date)
+        cvMealPlannerCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                // Store date
+                mYear = year;
+                mMonth = month;
+                mDayOfMonth = dayOfMonth;
+                // Show meal plan for that day (in case it has already been planned)
+                showMealPlan();
+                // Update text-string date
+                tvMealPlannerDate.setText(DateUtils.formatDate(mYear,mMonth,mDayOfMonth,mContext));
+            }
+        });
+    }
+
+    private void updateCalendarView() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(mYear, mMonth, mDayOfMonth, 0, 0);
+        cvMealPlannerCalendarView.setDate(calendar.getTimeInMillis());
+    }
+
+    private void showMealPlan() {
+
+        // TODO Min API: 19 - Look for alternatives
+        LinkedHashMap<String,Recipe> mealRecipeList = new LinkedHashMap<>();
+
+        // Add planned meals for the choosen day
+        if(mPlannedMealList != null && !mPlannedMealList.isEmpty()) {
+            for(PlannedMeal plannedMeal : mPlannedMealList) {
+                if (plannedMeal.getDayOfMonth() == mDayOfMonth
+                        && plannedMeal.getMonth() == mMonth
+                        && plannedMeal.getYear() == mYear) {
+                    // Look for planned recipe using recipeId
+                    if(mPlannedRecipeList != null && !mPlannedRecipeList.isEmpty()) {
+                        for (Recipe plannedRecipe : mPlannedRecipeList) {
+                            if (plannedMeal.getRecipeID().equals(plannedRecipe.getId())) {
+                                // Add planned recipe for the choosen day and mealtime
+                                mealRecipeList.put(plannedMeal.getMealTime(), plannedRecipe);
+                                continue;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Populate Mealtime Plan Adapter
+        if(mealRecipeList.size() == 0) { return; }
+        mMealPlannerMealtimeAdapter = new MealPlannerMealtimeAdapter(mealRecipeList, this);
+        rvCvMealPlannerMealtimeData.setAdapter(mMealPlannerMealtimeAdapter);
+        mMealPlannerMealtimeAdapter.notifyDataSetChanged();
+
+    }
+
+    //--------------------------------------------------------------------------------|
+    //                               DataModel Methods                                |
+    //--------------------------------------------------------------------------------|
+
+    /**
+     * Registers the View into PlannedMeal ViewModel to receive/update planned meals
+     */
+    private void registerToPlannedMealViewModel() {
+        // Create RecipeViewModel Factory for param injection
+        PlannedMealViewModel.Factory plannedMealFactory = new PlannedMealViewModel.Factory(
+                getActivity().getApplication());
+        // Get instance of RecipeViewModel
+        mPlannedMealViewModel = ViewModelProviders.of(this, plannedMealFactory)
+                .get(PlannedMealViewModel.class);
+    }
+
+    /**
+     * Registers the View into PlannedRecipe ViewModel to receive/update planned recipes
+     */
+    private void registerToPlannedRecipeViewModel() {
+        // Create RecipeViewModel Factory for param injection
+        PlannedRecipeViewModel.Factory plannedRecipeFactory = new PlannedRecipeViewModel.Factory(
+                getActivity().getApplication());
+        // Get instance of RecipeViewModel
+        mPlannedRecipeViewModel = ViewModelProviders.of(this, plannedRecipeFactory)
+                .get(PlannedRecipeViewModel.class);
+    }
+
+    private void enableNotificationsFromPlannedMealViewModel() {
+        mPlannedMealViewModel.getPlannedMeals().observe(this, new Observer<List<PlannedMeal>>() {
+            @Override
+            public void onChanged(@Nullable List<PlannedMeal> plannedMeals) {
+                if(plannedMeals == null || plannedMeals.isEmpty()) {
+                    return;
+                } else {
+                    mPlannedMealList = plannedMeals;
+                }
+                hasPlannedMealsRetrieved = true;
+                if(hasPlannedMealsRetrieved && hasPlannedRecipesRetrieved) {
+                    showMealPlan();
+                }
+            }
+        });
+    }
+
+    private void enableNotificationsFromPlannedRecipeViewModel() {
+        mPlannedRecipeViewModel.getPlannedRecipes().observe(this, new Observer<List<Recipe>>() {
+            @Override
+            public void onChanged(@Nullable List<Recipe> plannedRecipes) {
+                if(plannedRecipes == null || plannedRecipes.isEmpty()) {
+                    return;
+                } else {
+                    mPlannedRecipeList = plannedRecipes;
+                }
+                hasPlannedRecipesRetrieved = true;
+                if(hasPlannedMealsRetrieved && hasPlannedRecipesRetrieved) {
+                    showMealPlan();
+                }
+            }
+        });
     }
 
 }
--- app/src/main/java/com/udacity/mregtej/mymealplanner/ui/adapters/MealPlannerMealtimeAdapter.java
+++ app/src/main/java/com/udacity/mregtej/mymealplanner/ui/adapters/MealPlannerMealtimeAdapter.java
@@ -2,19 +2,32 @@ package com.udacity.mregtej.mymealplanner.ui.adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.udacity.mregtej.mymealplanner.R;
 import com.udacity.mregtej.mymealplanner.datamodel.Recipe;
+import com.udacity.mregtej.mymealplanner.ui.utils.UrlUtils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MealPlannerMealtimeAdapter extends RecyclerView.Adapter<MealPlannerMealtimeAdapter.ViewHolder> {
 
-    List<Recipe> mMealDayRecipeList;
+    LinkedHashMap<String,Recipe> mMealDayRecipeList;
     private Context mContext;
     private MealPlannerMealtimeClickListener mMealPlannerMealtimeClickListener;
 
@@ -23,8 +36,9 @@ public class MealPlannerMealtimeAdapter extends RecyclerView.Adapter<MealPlanner
     //                                 Constructors                                   |
     //--------------------------------------------------------------------------------|
 
-    public MealPlannerMealtimeAdapter(List<Recipe> recipes, MealPlannerMealtimeClickListener listener) {
-        this.mMealDayRecipeList = recipes;
+    public MealPlannerMealtimeAdapter(LinkedHashMap<String,Recipe> mealRecipes,
+                                      MealPlannerMealtimeClickListener listener) {
+        this.mMealDayRecipeList = mealRecipes;
         this.mMealPlannerMealtimeClickListener = listener;
     }
 
@@ -47,14 +61,17 @@ public class MealPlannerMealtimeAdapter extends RecyclerView.Adapter<MealPlanner
     public void onBindViewHolder(@NonNull MealPlannerMealtimeAdapter.ViewHolder holder,
                                  int position) {
 
-        // Retrieve i-ingredient from shopping ingredient list
-        Recipe recipe = mMealDayRecipeList.get(position);
+        // Retrieve
+        Set<Map.Entry<String, Recipe>> mealTimeSet = mMealDayRecipeList.entrySet();
+        Map.Entry<String, Recipe> mealTimeElement = (new ArrayList<Map.Entry<String, Recipe>>(mealTimeSet)).get(position);
+        String mealtime = mealTimeElement.getKey();
+        Recipe recipe = mealTimeElement.getValue();
 
         // Set position-tag
         holder.mealtimeViewLayout.setTag(position);
 
         // Populate UI elements
-        populateUIView(holder, recipe);
+        populateUIView(holder, mealtime, recipe);
 
     }
 
@@ -68,7 +85,7 @@ public class MealPlannerMealtimeAdapter extends RecyclerView.Adapter<MealPlanner
     //                             Getters / Setters                                  |
     //--------------------------------------------------------------------------------|
 
-    public List<Recipe> getmMealDayRecipeList() {
+    public HashMap<String, Recipe> getmMealDayRecipeList() {
         return mMealDayRecipeList;
     }
 
@@ -79,11 +96,21 @@ public class MealPlannerMealtimeAdapter extends RecyclerView.Adapter<MealPlanner
 
     class ViewHolder extends RecyclerView.ViewHolder {
 
+        @BindView(R.id.tv_mealtime_title)
+        TextView mealTimeTitle;
+        @BindView(R.id.iv_mealtime_recipe_update)
+        ImageView mealTimeRecipeUpdate;
+        @BindView(R.id.iv_mealtime_recipe_photo)
+        ImageView mealTimeRecipePhoto;
+        @BindView(R.id.tv_mealtime_recipe_name)
+        TextView mealTimeRecipeName;
+
         private final View mealtimeViewLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
             mealtimeViewLayout = itemView;
+            ButterKnife.bind(this, itemView);
         }
 
     }
@@ -107,8 +134,19 @@ public class MealPlannerMealtimeAdapter extends RecyclerView.Adapter<MealPlanner
      * @param holder        ViewHolder (View container)
      * @param recipe        Recipe object
      */
-    private void populateUIView(MealPlannerMealtimeAdapter.ViewHolder holder, Recipe recipe) {
-        // TODO fill-in
+    private void populateUIView(MealPlannerMealtimeAdapter.ViewHolder holder, String mealtime, Recipe recipe) {
+        holder.mealTimeTitle.setText(mealtime);
+        // TODO Implement recipe update button
+        // TODO Handle retrieved empty/null recipes
+        String imageUrl = recipe.getImageUrl();
+        if(imageUrl != null && !imageUrl.isEmpty() && UrlUtils.isValid(imageUrl)) {
+            Picasso.get()
+                    .load(imageUrl)
+                    .error(ContextCompat.getDrawable(mContext, R.drawable.im_recipe))
+                    .fit()
+                    .into(holder.mealTimeRecipePhoto);
+        }
+        holder.mealTimeRecipeName.setText(recipe.getTitle());
     }
 
     //--------------------------------------------------------------------------------|
